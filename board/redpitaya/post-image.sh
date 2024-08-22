b3964d9ef6bdc313ea5fe8b6068d19fd4cc26d02
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2024-08-22 13:29:24 +0200

    board/redpitaya/post-image.sh: fixed sed expression with xilinx/ directory

--- board/redpitaya/post-image.sh
+++ board/redpitaya/post-image.sh
@@ -3,7 +3,7 @@
 # include the linux device tree name corresponding to the desired
 # configuration to the genimage configuration file
 
-DTB="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([\/a-z0-9_ \-]*\)"$/\1/p' ${BR2_CONFIG})"
+DTB="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="xilinx\/\([\/a-z0-9_ \-]*\)"$/\1/p' ${BR2_CONFIG})"
 BOARD_PATH="$(dirname $0)"
 
 GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
